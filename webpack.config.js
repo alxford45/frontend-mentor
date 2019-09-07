355ffbecfed7f6c0693cf876ebfd836f4378dd1a
Alex <alxford45@gmail.com>
2019-09-07 16:43:13 -0500

    added basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HmtlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    host: "localhost",
+    port: 8080,
+    hot: false,
+    publicPath: "./assets",
+    contentBase: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        loaders: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        exclude: /node_modules/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HmtlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
